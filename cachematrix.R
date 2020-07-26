9b3a74179eb68c9e77ba06754e4011b75d101783
Godspeed567 <Godspeed567@users.noreply.github.com>
2020-07-27 00:49:34 +0530

    Update cachematrix.R
    completed assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,57 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Assignment: Caching the Inverse of a Matrix
+## A pair of function that catch the mean if it has already been computed if not the function will compute and return it.
 
-## Write a short comment describing this function
+## makeCacheMatrix: This function creates a special "matrix" object that can cache its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
+        
+        ##initialising inverse matrix
+        m <- NULL
+        
+        ##set the value of the matrix
+        set <- function(matrix) {
+                x <<- matrix
+                m <<- NULL
+        }
+        
+        ##get the value of the matrix
+        get <- function() x
+        
+        ##set the inverse of the matrix
+        setinverse <- function(inverse) m <<- inverse
+        
+        ##get the inverse of the matrix
+        getinverse <- function() m
+        
+        
+        list(set = set, get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
 
 }
 
 
-## Write a short comment describing this function
+## cacheSolve: This function computes the inverse of the special "matrix" returned by makeCacheMatrix above. 
+##If the inverse has already been calculated (and the matrix has not changed), then the cachesolve should retrieve
+##the inverse from the cache
 
 cacheSolve <- function(x, ...) {
+        
+        m <- x$getinverse()
+        
+        ##return inverse if already set
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        
+        data <- x$get()
+        
+        ##computing inverse
+        m <- solve(data, ...)
+        
+        x$setinverse(m)
+        
         ## Return a matrix that is the inverse of 'x'
+        m
 }
